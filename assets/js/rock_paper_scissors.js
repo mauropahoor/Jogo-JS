c7f4e94247048b09f5c2f1c3b48a2072fcdd4b99
unknown <mauro.pahoor@gmail.com>
2022-04-11 22:18:48 -0300

    Jogo da velha

---
+++ assets/js/rock_paper_scissors.js
@@ -0,0 +1,27 @@
+function start_game(){
+    let user_option = document.getElementsByName('user_option');
+    let options = ['pedra', 'papel', 'tesoura'];
+    let computer_option = options[Math.floor(Math.random() * 3)];
+    let result = document.getElementById('result');
+    
+    let user_score = parseInt(document.getElementById('user_score').value);
+    let computer_score = parseInt(document.getElementById('computer_score').value);
+    for(i = 0; i < user_option.length; i++){
+        if(user_option[i].checked){
+            user_option = user_option[i].value;
+            if((user_option == "pedra" && computer_option == "pedra") || (user_option == "papel" && computer_option == "papel") || (user_option == "tesoura" && computer_option == "tesoura")){
+                result.innerHTML = "Vou jogar " + computer_option + "!<br>Logo, empatamos.";
+            }
+            else if ((user_option == "pedra" && computer_option == "papel") || (user_option == "papel" && computer_option == "tesoura") || (user_option == "tesoura" && computer_option == "pedra")){
+                result.innerHTML = "Vou jogar " + computer_option + "!<br>Logo, você perdeu.";
+                document.getElementById("computer_score").setAttribute("value", computer_score += 1); //Adicionar pontuação
+                document.getElementById('computer_scoreboard').innerHTML = computer_score;
+            }
+            else if ((user_option == "pedra" && computer_option == "tesoura") || (user_option == "papel" && computer_option == "pedra") || (user_option == "tesoura" && computer_option == "papel")){
+                result.innerHTML = "Vou jogar " + computer_option + "!<br>Logo, você ganhou.";
+                document.getElementById("user_score").setAttribute("value", user_score += 1); //Adicionar pontuação
+                document.getElementById('user_scoreboard').innerHTML = user_score;
+            }
+        }
+    }
+}
---
+++ assets/js/tic_tac_toe.js
@@ -0,0 +1,82 @@
+function game(id){
+    let move = document.getElementById(id);
+    let round = parseInt(document.getElementById("round").value);
+    let winner = document.getElementById("winner");
+
+    if(move.getAttribute("value") == 0){ //Don't make the game be able to repeat a move
+        if(round % 2 == 0){ //Check if have to use X or O (odd==X)(even=O)
+            move.setAttribute("src", "assets/img/o.png")
+            move.setAttribute("value", "o");
+            document.getElementById('round').setAttribute('value', round += 1);
+        }
+        else if(round % 2 != 0){
+            move.setAttribute("src", "assets/img/x.png");
+            move.setAttribute("value", "x");
+            document.getElementById('round').setAttribute('value', round += 1);
+        }
+    }
+    
+    let matrix_size = 3;
+    let position = new Array(matrix_size); //Create matrix with the moves
+
+    for (i = 0; i < position.length; i++){
+        position[i] = new Array(matrix_size);
+    }
+    for (i = 0; i < matrix_size; i++){
+        for(j = 0; j < matrix_size; j++){
+            let content = i.toString() + j.toString(); //Method to identify each ID in HTML and create an matrice with the moves
+            position[i][j] = document.getElementById(content).getAttribute("value");
+        }
+    }
+    for ( i = 0; i < matrix_size; i++) {
+        for (j = 0; j < matrix_size; j++){
+            //Check if O or X win
+            if((position[i][j] == 'x' && position[i][j + 1] == 'x' && position[i][j + 2] == 'x') || (position[i][j] == 'o' && position[i][j + 1] == 'o' && position[i][j + 2] == 'o')){
+                if(position[i][j] == 'x')
+                    winner.innerHTML = "O vencedor foi o X!";
+                else
+                    winner.innerHTML = "O vencedor foi o O!";
+            }
+            else if((position[i][j] == 'x' && position[i + 1][j] == 'x' && position[i + 2][j] == 'x') || (position[i][j] == 'o' && position[i + 1][j] == 'o' && position[i + 2][j] == 'o')){
+                if(position[i][j] == 'x')
+                    winner.innerHTML = "O vencedor foi o X!";
+                else
+                    winner.innerHTML = "O vencedor foi o O!";
+            }
+            else if((position[i][j] == 'x' && position[i + 1][j + 1] == 'x' && position[i + 2][j + 2] == 'x') || (position[i][j] == 'o' && position[i + 1][j + 1] == 'o' && position[i + 2][j + 2] == 'o')){
+                if(position[i][j] == 'x')
+                    winner.innerHTML = "O vencedor foi o X!";
+                else
+                    winner.innerHTML = "O vencedor foi o O!";
+            }
+            else if((position[i][j] == 'x' && position[i + 1][j - 1] == 'x' && position[i + 2][j - 2] == 'x') || (position[i][j] == 'o' && position[i + 1][j - 1] == 'o' && position[i + 2][j - 2] == 'o')){
+                if(position[i][j] == 'x')
+                    winner.innerHTML = "O vencedor foi o X!";
+                else
+                    winner.innerHTML = "O vencedor foi o O!";
+            }
+        }
+    }
+    let tie = 0;
+    for ( i = 0; i < matrix_size; i++) {
+        for (j = 0; j < matrix_size; j++){
+            if(position[i][j] == 'x' || position[i][j] == 'o')
+                tie++; 
+        }
+    }
+    if(tie == 9)
+        alert("VAMO");
+}
+function restart(){
+    let game_size = 3;
+    let winner = document.getElementById("winner");
+    winner.innerHTML = ""; //Reset the winning text
+
+    for (i = 0; i < game_size; i++){
+        for(j = 0; j < game_size; j++){
+            let content = i.toString() + j.toString();
+            document.getElementById(content).setAttribute('value', '0'); //Reset the value in matrice and the image
+            document.getElementById(content).setAttribute('src', 'assets/img/empty.png');
+        }
+    }
+}
