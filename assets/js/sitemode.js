d3e32eb2d094c28d337f40caceea61ecc4692c95
unknown <mauro.pahoor@gmail.com>
2022-04-24 19:10:00 -0300

    Better Code

--- assets/js/sitemode.js
+++ assets/js/sitemode.js
@@ -2,7 +2,7 @@ function changeMode(){
 
     let mode = document.getElementById("site_theme");
     let mode_value = mode.getAttribute("value");
-    theme = document.getElementById("css_theme");
+    let theme = document.getElementById("css_theme");
     if(mode_value == "lightmode"){
         theme.setAttribute("href", "assets/css/dark_theme.css");
         mode.setAttribute("value", "darkmode");
@@ -14,8 +14,7 @@ function changeMode(){
 }
 function computer_theme(){ //Identify computer theme and use in site
     let mode = document.getElementById("site_theme");
-    let mode_value = mode.getAttribute("value");
-    theme = document.getElementById("css_theme");
+    let theme = document.getElementById("css_theme");
     const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
     if (darkThemeMq.matches) {
         theme.setAttribute("href", "assets/css/dark_theme.css");
